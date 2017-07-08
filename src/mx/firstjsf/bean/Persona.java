77496f6e921de6cd34594713026901431a722520
crack81 <crack811@hotmail.com>
2017-07-07 20:07:47 -0500

    bean persona agregado

---
+++ src/mx/firstjsf/bean/Persona.java
@@ -0,0 +1,35 @@
+package mx.firstjsf.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class Persona implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nombre;
+	private String sexo;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	
+
+}
